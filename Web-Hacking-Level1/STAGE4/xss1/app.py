ac70ec8751954d07e90a71827b9d847641ea17f5
Jeong Min Kim <dsaqaz421@naver.com>
2022-07-11 04:24:48 +0000

    init

---
+++ Web-Hacking-Level1/STAGE4/xss1/app.py
@@ -0,0 +1,94 @@
+'''
+!!solution!!
+- /vuln route는 xss 필터링이 안걸려있는 URL임
+- /memo는 지금까지 입력된 텍스트들을 보여주고 있음
+- /flag는 selenium을 통해 url 호출을 해줌
+
+1. document.cookie를 통해 flag 값을 파악해야함
+2. /flag에서 input 값으로 /vuln의 xss 공격과 /memo에서 기록하는 걸 이용..
+3. location.href를 통해 /memo로 cookie를 넘기도록 submit
+    - <script>location.href="http://127.0.0.1:8000/memo?memo=" + document.cookie</script>
+4. 정상적으로 처리했으면 /memo에 기록된 flag 값 확인 가능
+'''
+
+#!/usr/bin/python3
+from flask import Flask, request, render_template
+from selenium import webdriver
+import urllib
+import os
+
+app = Flask(__name__)
+app.secret_key = os.urandom(32)
+
+try:
+    FLAG = open("./flag.txt", "r").read()
+except:
+    FLAG = "[**FLAG**]"
+
+
+def read_url(url, cookie={"name": "name", "value": "value"}):
+    cookie.update({"domain": "127.0.0.1"})
+    try:
+        options = webdriver.ChromeOptions()
+        for _ in [
+            "headless",
+            "window-size=1920x1080",
+            "disable-gpu",
+            "no-sandbox",
+            "disable-dev-shm-usage",
+        ]:
+            options.add_argument(_)
+        driver = webdriver.Chrome("/chromedriver", options=options)
+        driver.implicitly_wait(3)
+        driver.set_page_load_timeout(3)
+        driver.get("http://127.0.0.1:8000/")
+        driver.add_cookie(cookie)
+        driver.get(url)
+    except Exception as e:
+        driver.quit()
+        # return str(e)
+        return False
+    driver.quit()
+    return True
+
+
+def check_xss(param, cookie={"name": "name", "value": "value"}):
+    url = f"http://127.0.0.1:8000/vuln?param={urllib.parse.quote(param)}"
+    return read_url(url, cookie)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/vuln")
+def vuln():
+    param = request.args.get("param", "")
+    return param
+
+
+@app.route("/flag", methods=["GET", "POST"])
+def flag():
+    if request.method == "GET":
+        return render_template("flag.html")
+    elif request.method == "POST":
+        param = request.form.get("param")
+        if not check_xss(param, {"name": "flag", "value": FLAG.strip()}):
+            return '<script>alert("wrong??");history.go(-1);</script>'
+
+        return '<script>alert("good");history.go(-1);</script>'
+
+
+memo_text = ""
+
+
+@app.route("/memo")
+def memo():
+    global memo_text
+    text = request.args.get("memo", "")
+    memo_text += text + "\n"
+    return render_template("memo.html", memo=memo_text)
+
+
+app.run(host="0.0.0.0", port=8000)
