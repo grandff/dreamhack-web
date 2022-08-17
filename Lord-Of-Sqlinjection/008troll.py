ecbfecafaa33fa603f53ec794c9ca29b100f5864
Jeong Min Kim <dsaqaz421@naver.com>
2022-08-17 23:38:22 +0000

    Feat: los troll write up

--- Lord-Of-Sqlinjection/008troll.py
+++ Lord-Of-Sqlinjection/008troll.py
@@ -3,12 +3,9 @@ from urllib import parse
 
 # single quote 사용 불가, admin 키워드 사용 불가
 
-host = "https://los.rubiya.kr/chall/troll_05b5eb65d94daf81c42dd44136cb0063.php?pw="
+host = "https://los.rubiya.kr/chall/troll_05b5eb65d94daf81c42dd44136cb0063.php?id="
 cookies = {"PHPSESSID" : "8s7khiq67rue3qlsffsskni093"}
 password_length = 0
 s = requests.Session()
 
-param = ""
-
-#97 100 109 105 110
-# 큰따옴표사용
+param = "ADMIN" # 대소문자 구분
