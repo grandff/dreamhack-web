b7f71babd90dbc0c5876e45a34501c6d76867087
Jeong Min Kim <dsaqaz421@naver.com>
2022-09-07 03:43:09 +0000

    Feat: google ctf secret location write up

---
+++ Lord-Of-Sqlinjection/014giant.py
@@ -0,0 +1,18 @@
+'''
+<?php 
+  include "./config.php"; 
+  login_chk(); 
+  $db = dbconnect(); 
+  if(strlen($_GET[shit])>1) exit("No Hack ~_~"); 
+  if(preg_match('/ |\n|\r|\t/i', $_GET[shit])) exit("HeHe"); 
+  $query = "select 1234 from{$_GET[shit]}prob_giant where 1"; 
+  echo "<hr>query : <strong>{$query}</strong><hr><br>"; 
+  $result = @mysqli_fetch_array(mysqli_query($db,$query)); 
+  if($result[1234]) solve("giant"); 
+  highlight_file(__FILE__); 
+?>
+'''
+
+from re import I
+import requests
+from urllib import parse
